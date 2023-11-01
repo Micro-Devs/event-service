b51ddf783695eccdedac50e636a42dfbde484bde
Muhammet Emre Vatan <emre.vatan98@hotmail.com>
2023-11-02 02:06:16 +0300

    *base package integration

--- src/main/java/com/microdevs/eventservice/EventServiceApplication.java
+++ src/main/java/com/microdevs/eventservice/EventServiceApplication.java
@@ -1,11 +1,16 @@
 package com.microdevs.eventservice;
 
+import com.microdevs.baseservice.exception.GlobalExceptionHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableEurekaClient
+@ComponentScan(basePackages = "com.microdevs.*")
+@Import(GlobalExceptionHandler.class)
 public class EventServiceApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/microdevs/eventservice/data/entity/EventEntity.java
@@ -0,0 +1,6 @@
+package com.microdevs.eventservice.data.entity;
+
+import com.microdevs.baseservice.service.AuditModelBase;
+
+public class EventEntity extends AuditModelBase {
+}
